e46fbab034f04ff6438b5107b2b0c363461aa275
seralekseenko <seralekseenko@ukr.net>
2018-10-03 08:44:15 +0300

    refactor code

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ import javafx.util.Pair;
 public class Main {
 
     public static void main(String[] args) {
-	    //Create a data structure to simulate a sorted log file.
+	    // Create a data structure to simulate a sorted log file.
         // Cell number = the time of the formation of friendly relations between couples.
         Pair[] logTime = {
                 //Who made friends at this moment?
-                new Pair<>(3, 7),//moment 0
-                new Pair<>(3, 8),//moment 1
-                new Pair<>(6, 12),//moment 2
-                new Pair<>(0, 2),//moment 3
-                new Pair<>(1, 4),//moment ...
+                new Pair<>(3, 7),// moment 0
+                new Pair<>(3, 8),// moment 1
+                new Pair<>(6, 12),// moment 2
+                new Pair<>(0, 2),// moment 3
+                new Pair<>(1, 4),// moment ...
                 new Pair<>(1, 3),
                 new Pair<>(2, 5),
                 new Pair<>(2, 6),
@@ -22,23 +22,22 @@ public class Main {
                 new Pair<>(6, 13),
                 new Pair<>(4, 14)
         };
-        int nMembers = 15;//Including member number 0.
+        int nMembers = 15;// Including member number 0.
 
         UnionFind unionFind = new UnionFind(nMembers);
 
-
-        int LOG_TIME_LENGTH = logTime.length;
-        //Also "i" is equal to the timestamp.
-        for (int i = 0; i < LOG_TIME_LENGTH; i++) {
+        int logTimeLength = logTime.length;
+        // Also "i" is equal to the timestamp.
+        for (int i = 0; i < logTimeLength; i++) {
             System.out.println("Time is: " +i);
-            //According to the time log, I invoke the merge method.
-            //The merge method returns the size of the merged tree.
+            // According to the time log, I invoke the merge method.
+            // The merge method returns the size of the merged tree.
             int sizeTree = unionFind.union((Integer) logTime[i].getKey(), (Integer) logTime[i].getValue());
 
-            //If, after the current merge,
+            // If, after the current merge,
             // the size of the tree is equal to the number of network members,
             // then all network members are connected.
-            if (nMembers == sizeTree){
+            if (nMembers == sizeTree) {
                 System.out.println("Completeness occurs when time is equal to: " +i);
                 System.out.println("sizeTree: " +sizeTree);
                 System.out.println("nMembers: " +nMembers);
--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,39 +1,39 @@
 class UnionFind {
 
-    private int[] id;
-    private int[] treeSize;
+    private final int[] id;
+    private final int[] treeSize;
 
-    UnionFind(int n){
+    UnionFind(int n) {
         id = new int[n];
         treeSize = new int[n];
         for (int i = 0; i < n; i++) {
             id[i] = i;
             treeSize[i] = 1;
-
         }
     }
 
-    private int root(int i){
-        while(i != id[i]){
+    private int root(int i) {
+        while (i != id[i]) {
             id[i] = id[id[i]];
             i = id[i];
         }
         return i;
     }
-//I think this method should return the current size of the tree.
-    int union(int p, int q){
+
+    // I think this method should return the current size of the tree.
+    int union(int p, int q) {
         int i = root(p);
         int j = root(q);
 
         if (i == j) return treeSize[i];
-        if  (treeSize[i] < treeSize[j]) {
+        if (treeSize[i] < treeSize[j]) {
             id[i] = j;
             treeSize[j] += treeSize[i];
-            return treeSize[j];//The current size of the tree.
-        }  else{
+            return treeSize[j];// The current size of the tree.
+        } else {
             id[j] = i;
             treeSize[i] += treeSize[j];
-            return treeSize[i];//The current size of the tree.
+            return treeSize[i];// The current size of the tree.
         }
     }
 }
